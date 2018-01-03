62d703784ec6144a84fdc341a61a5d6a24eae1ad
wenyan4work <wenyan4work@gmail.com>
2018-01-03 13:33:40 -0500

    add multi sphere pack&unpack test

--- Test/Buffer/testBuffer.cpp
+++ Test/Buffer/testBuffer.cpp
@@ -1,11 +1,11 @@
 
-#include<iostream>
 #include "../../Buffer.hpp"
 #include "../../Sphere.hpp"
+#include <iostream>
 
-void testSimple(){
+void testSimple() {
     std::string name("SPHERE");
-    std::array<double,4> array4 {1,2,3,4};
+    std::array<double, 4> array4{1, 2, 3, 4};
     Buffer mybuffer;
     mybuffer.pack(name);
     mybuffer.pack(array4);
@@ -14,16 +14,16 @@ void testSimple(){
 
     mybuffer.unpack(name);
     mybuffer.unpack(array4);
-    std::cout<<name<< " "<<array4[0]<<array4[1]<<array4[2]<<array4[3]<<std::endl;
+    std::cout << name << " " << array4[0] << array4[1] << array4[2] << array4[3] << std::endl;
 }
 
-void testSingleSphere(){
+void testSingleSphere() {
     Buffer mybuffer;
     {
-        Sphere sphere(5,1.0,1.5,Evec3(1,2,3),Equatn(1,0,0,0));
-        sphere.addLayer(std::string("testLayer"),SPHExp::KIND::STKDL,4);
-        auto &specValue=sphere.sphLayer["testLayer"]->spectralCoeff;
-        std::fill(specValue.begin(),specValue.end(),1.0);
+        Sphere sphere(5, 1.0, 1.5, Evec3(1, 2, 3), Equatn(1, 0, 0, 0));
+        sphere.addLayer(std::string("testLayer"), SPHExp::KIND::STKDL, 4);
+        auto &specValue = sphere.sphLayer["testLayer"]->spectralCoeff;
+        std::fill(specValue.begin(), specValue.end(), 1.0);
         sphere.pack(mybuffer);
     }
     Sphere sphere2;
@@ -32,8 +32,35 @@ void testSingleSphere(){
     sphere2.dumpLayer(std::string("testLayer"));
 }
 
-int main(){
+void testMultiSphere() {
+    Buffer mybuffer;
+    { // pack sphere 1
+        Sphere sphere(5, 1.0, 1.5, Evec3(1, 2, 3), Equatn(1, 0, 0, 0));
+        sphere.addLayer(std::string("testLayer"), SPHExp::KIND::STKDL, 4);
+        auto &specValue = sphere.sphLayer["testLayer"]->spectralCoeff;
+        std::fill(specValue.begin(), specValue.end(), 1.0);
+        sphere.pack(mybuffer);
+    }
+    { // pack sphere 2
+        Sphere sphere(8, 2.0, 3.5, Evec3(3, 2, 1), Equatn(0, 1, 0, 0));
+        sphere.addLayer(std::string("testLayer"), SPHExp::KIND::LAPDL, 3);
+        auto &specValue = sphere.sphLayer["testLayer"]->spectralCoeff;
+        std::fill(specValue.begin(), specValue.end(), 2.0);
+        sphere.pack(mybuffer);
+    }
+    Sphere sphere1;
+    sphere1.unpack(mybuffer);
+    sphere1.dumpSphere();
+    sphere1.dumpLayer(std::string("testLayer"));
+    Sphere sphere2;
+    sphere2.unpack(mybuffer);
+    sphere2.dumpSphere();
+    sphere2.dumpLayer(std::string("testLayer"));
+}
+
+int main() {
     testSimple();
     testSingleSphere();
-    return 0;    
+    testMultiSphere();
+    return 0;
 }
