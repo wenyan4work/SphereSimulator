e6efb6c1a5a8d375ba42f25d5965e178849a556a
wenyan4work <wenyan4work@gmail.com>
2018-01-08 08:13:18 -0500

    save working progress

--- NBNode.hpp
+++ NBNode.hpp
@@ -13,12 +13,13 @@
 #include <vector>
 
 #include "Buffer.hpp"
+#include "MortonID.hpp"
 
-#define MAXTREEDEPTH 15
+#define MAX_DEPTH_NB 15
+// current  implementation 3D only
+#define COLLEAGUECOUNT 27
 
-
-// requires c++11 variadic template
-template <typename... ObjTypes> // e.g. MyClass<T1, T2>
+template <typename ObjT1, typename ObjT2> // e.g. MyClass<T1, T2>
 class NBNode {
     // the node for octree, holding multiple types of objects
 
@@ -33,28 +34,46 @@ class NBNode {
     NBNode *parent; // Pointer to parent node
     NBNode **child; // Pointer child nodes, maximum 8 pointers
 
-    size_t max_pts;
-    long long weight;
+    size_t maxPts;    // max number of points in all its descendent
+    long long weight; // weight for load balancing across mpi ranks
+    // weight should be the same as used in PVFMM to generate octree with similar structure
 
-    Real_t coord[COORD_DIM];
-    MPI_Node<Real_t> *colleague[COLLEAGUE_COUNT];
-    Vector<char> packed_data;
+    double coord[3]; // the coord (x,y,z) of this node TODO: check coord is center or the vertex with lower xyz
+    NBNode<double> *colleague[COLLEAGUECOUNT]; // pointer to its neighbor nodes.
 
-    std::tuple<std::vector<ObjTypes> *...> objContainerPtrs; // pointers to container of objects
-    std::array<std::vector<size_t>, sizeof...(ObjTypes)>
-        objIndexArray;              // indices of objs owned by this node (including its child nodes)
-    std::vector<size_t> pointIndex; // the index of objs in the fist container, used for tree subdivision
-    std::vector<double> pointCoord; // the coordinate of objs in the first container, used for tree subdivision
+    // length scale and coordinate base should be the same for all nodes in a tree across all mpi ranks
+    // double lengthScale; // the octree is [0,1)^3, multiply this to get the real length
+    // double coordBase[3]; // the octree is [0,1)^3, add this to get the real coordinate
+
+    std::vector<ObjT1> *objT1ContainerPtr; // pointer to container of objects
+    std::vector<size_t> objT1Index;        // indices of objs owned by this node (non-empty only if it is a leaf node)
+    std::vector<double> objT1Coord;        // the coordinate of objs in the first container, used for tree subdivision
+    std::vector<size_t> objT1scatter;      // scatter index
+
+    std::vector<ObjT2> *objT2ContainerPtr; // pointer to container of objects of Type 2, = nullptr if T2 is void
+    std::vector<size_t> objT2Index;        // empty if T2 is void
+    std::vector<size_t> objT2scatter;      // scatter index
 
     Buffer packedData; // packed data for transfer over MPI
 
     // constructor
-    NBNode(int maxDepth_, std::tuple<std::vector<ObjTypes> *...> & objContainerPtrs_) : maxDepth(maxDepth_<MAXTREEDEPTH?maxDepth_,MAXTREEDEPTH), 
-    parent(nullptr), child(nullptr),objContainerPtrs(objContainerPtrs), status(1) {
-
+    NBNode(int maxDepth_, int maxPts_, std::vector<ObjT1> *objT1ContainerPtr_,
+           std::vector<ObjT2> *objT2ContainerPtr_ = nullptr)
+        : maxDepth(maxDepth_ < MAX_DEPTH_NB ? maxDepth_ : MAX_DEPTH_NB), maxPts(maxPts_), parent(nullptr),
+          child(nullptr), status(1), ghost(false), depth(0), path2node(0), weight(0), coord({0, 0, 0}),
+          colleague(nullptr), node_id(0) {
+        objT1ContainerPtr = objT1ContainerPtr_;
+        if (std::is_void<ObjT2>::value == true) {
+            objT2ContainerPtr = nullptr;
+        } else {
+            // objT2ContainerPtr = objT2ContainerPtr_;
+            // TODO: octree with two species is not implemented yet.
+            printf("Octree with two species is not implemented yet.");
+            exit(1);
+        }
     }
 
-    // destructor
+    // destructor. TODO: destruct an octant without invalidating its parent's and children's pointers to it
     ~NBNode();
 
     // forbit copy
@@ -64,9 +83,9 @@ class NBNode {
     const NBNode &operator=(NBNode &&) = delete;
 
     // Initialize the node by passing the relevant data.
-    void Initialize(NBNode *parent_, int path2node_);
+    void initialize(NBNode *parent_, int path2node_);
 
-    void ClearData() {}
+    void clearData(); // TODO: implement this
 
     // Returns the dimension of the tree.
     int getDim() { return dim; }
@@ -86,9 +105,9 @@ class NBNode {
     /**
      * Returns the index which corresponds to this node among its
      * siblings (parent's children).
-     * this->getParent()->getChild(this->Path2Node())==this
+     * this->getParent()->getChild(this->getPath2Node())==this
      */
-    int Path2Node();
+    int getPath2Node();
 
     /**
      * Allocate a new object of the same type (as the derived class) and
@@ -125,89 +144,81 @@ class NBNode {
      * sorted and partitioned across MPI processes and the scatter index is
      * saved.
      */
-    void NodeDataVec(std::vector<Vector<Real_t> *> &coord, std::vector<Vector<Real_t> *> &value,
-                             std::vector<Vector<size_t> *> &scatter) {
-        coord.push_back(&pt_coord);
-        value.push_back(&pt_value);
-        scatter.push_back(&pt_scatter);
+    void getNodeDataVec(std::vector<std::vector<double> *> &coord, std::vector<std::vector<size_t> *> &scatter) {
+        coord.push_back(&objT1Coord);
+        scatter.push_back(&objT1scatter);
     }
-
-    /**
-     * \brief Clear node data.
-     */
-    virtual void ClearData();
 
     /**
      * \brief Returns the colleague corresponding to the input index.
      */
-    MPI_Node<Real_t> *Colleague(int index) { return colleague[index]; }
+    NBNode *getColleague(int index) { return colleague[index]; }
 
     /**
      * \brief Set the colleague corresponding to the input index.
      */
-    void SetColleague(MPI_Node<Real_t> *node_, int index) { colleague[index] = node_; }
+    void setColleague(NBNode *node_, int index) { colleague[index] = node_; }
 
     /**
      * \brief Returns the cost of this node. Used for load balancing.
      */
-    virtual long long &NodeCost() { return weight; }
+    long long &getNodeCost() { return weight; }
 
     /**
      * \brief Returns an array of size dim containing the coordinates of the
      * node.
      */
-    Real_t *Coord() {
-        assert(coord != NULL);
+    double *getCoord() {
+        assert(coord != nullptr);
         return coord;
     }
 
     /**
      * \brief Determines if the node is a Ghost node or not.
      */
-    bool IsGhost() { return ghost; }
+    bool isGhost() { return ghost; }
 
     /**
      * \brief Sets the ghost flag of this node.
      */
-    void SetGhost(bool x) { ghost = x; }
+    void setGhost(bool x) { ghost = x; }
 
     /**
      * \brief Gets Morton Id of this node.
      */
-    inline MortonId GetMortonId();
+    inline MortonId getMortonId();
 
     /**
      * \brief Sets the coordinates of this node using the given Morton Id.
      */
-    inline void SetCoord(MortonId &mid);
+    inline void setCoord(MortonId &mid);
 
     /**
-     * \brief Allocate a new object of the same type (as the derived class) and
-     * return a pointer to it type cast as (TreeNode*).
+     * \brief Allocate a new node and return a pointer to it.
      */
-    virtual TreeNode *NewNode(TreeNode *n_ = NULL);
+    NBNode *allocNewNode(TreeNode *n_ = nullptr);
 
     /**
      * \brief Evaluates and returns the subdivision condition for this node.
      * 'true' if node requires further subdivision.
      */
-    virtual bool SubdivCond();
+    bool getSubdivCond();
 
     /**
      * \brief Create child nodes and Initialize them.
      */
-    virtual void Subdivide();
+    void subdivide();
 
     /**
      * \brief Truncates the tree i.e. makes this a leaf node.
      */
-    virtual void Truncate();
+    void truncate();
 
     /**
      * \brief Pack this node to be transmitted to another process. The node
      * is responsible for allocating and freeing the memory for the actual data.
      */
-    virtual PackedData Pack(bool ghost = false, void *buff_ptr = NULL, size_t offset = 0);
+    packedData pack(bool ghost = false, size_t offset = 0);
 
     /**
      * \brief Initialize the node with data from another process.
@@ -226,12 +237,6 @@ class NBNode {
      */
     template <class VTUData_t, class Node_t>
     static void VTU_Data(VTUData_t &vtu_data, std::vector<Node_t *> &nodes, int lod);
-
-    Vector<Real_t> pt_coord;   // coordinates of points
-    Vector<Real_t> pt_value;   // value at points
-    Vector<size_t> pt_scatter; // scatter index mapping original data.
 };
-}
-;
 
 #endif
