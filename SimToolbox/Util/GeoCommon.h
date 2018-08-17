0bd913f2f901c63747f7d12bc6aed794b3eb0784
Wen Yan <wenyan4work@gmail.com>
2018-08-17 14:08:32 -0400

    using submodule for simtoolbox

--- SimToolbox/Util/GeoCommon.h
+++
@@ -1,9 +0,0 @@
-#ifndef GEOCOMMON_H_
-#define GEOCOMMON_H_
-
-#define GEO_INVALID_INDEX  (-1)
-#define GEO_DEFAULT_COLBUF (0.3)  // record collision block for sep < COLBUF*(rI+rJ)
-#define GEO_DEFAULT_RADIUS (1.2)
-#define GEO_DEFAULT_NEIGHBOR (2.0)
-
-#endif
